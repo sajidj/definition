6f506913dffbfcc82a4c9c486d4cfad51b86f98e
sajidj <sajidjaber@gmail.com>
2014-02-06 08:44:35 -0600

    def

---
+++ def.py
@@ -0,0 +1,32 @@
+class function():
+    def menu():
+        print 'Weclome'
+        print 'Choose operation:'
+        print '1) Addition'
+        print '2) Subtraction'
+        print '3) Multiplication'
+        print '4) Division'
+        a=raw_input("what operation: ")
+        print a
+    def add():
+        x=raw_input("First Number:")
+        y=raw_input("Second Number: ")
+        print x "+" y "=" x+y
+    def sub():
+            x=x=raw_input("First Number:")
+            y=raw_input("Second Number: ")
+            print x "-" y "=" x-y
+    def mult():
+            x=x=raw_input("First Number:")
+            y=raw_input("Second Number: ")
+            print x "*" y "=" x*y
+    def div():
+            x=x=raw_input("First Number:")
+            y=raw_input("Second Number: ")
+            print x "/" y "=" x/y
+
+            
+
+
+    
+                
